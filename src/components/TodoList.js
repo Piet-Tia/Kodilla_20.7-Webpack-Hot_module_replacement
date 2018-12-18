c00becd90074437acce369ecb8274a9e6908851a
Piotr Nastaj <piotr@nastaj.net>
2018-12-18 15:14:01 +0100

    Add HMR

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,18 +8,15 @@ class TodoList extends React.Component {
 	
 	passClickedItem(clickedItem){
 		this.props.onClick(clickedItem);
-		console.log('passClickedItem - clickedItem: ', clickedItem)
 	}
 	
 	render() {
 		const singleTaskNames = this.props.tasks.map(singleTask => {
-			console.log ('render: singleTask in map function: ', singleTask)
 			return (
 			<li key={singleTask.id} onClick={() => this.passClickedItem(singleTask.id)}>
 				{singleTask.text}
 			</li>
 		)});
-		console.log('render: singleTaskNames: ', singleTaskNames)
 		return (
 			<div>
 				Tasks:
